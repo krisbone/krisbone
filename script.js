b825950277c88c987a0899f8fe31b2e86f143e92
krisbone <137225617+krisbone@users.noreply.github.com>
2025-10-04 23:48:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wordRotates = document.getElementById('word-rotates');
+function manageClick() {
+    if (wordRotates.classList.contains('rotating-click')) {
+        return;
+    }
+
+    wordRotates.classList.add('rotating-click');
+    const animationTime = 300;
+    setTimeout(() => {
+        wordRotates.classList.remove('rotating-click');   
+    }, animationTime); 
+}
+
+wordRotates.addEventListener('click', manageClick);
+
+const imaget = document.getElementById('temblorwn')
+
+functionm manageImaget() {
+    imaget.classList.toggle('temblor-happens');
+}
+
+imaget.addEventListener('click', manageImaget);
